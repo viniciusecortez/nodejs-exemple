753809f64567033cdecbdfb8dd2d93c7e4645f79
Vinicius Eustaquio Cortez <viniciusecortez@outlook.com>
2019-09-12 20:55:48 -0300

    Server rodando

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const cors = require('cors');
+
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json({ limit: "5mb" }));
+
+// const rotasDeLivro = require("./routes/livros");
+
+// app.use('/livros', rotasDeLivro);
+
+app.listen(port, () => {
+    console.log(`Servidor está rodando na porta: ${port}`);
+})
