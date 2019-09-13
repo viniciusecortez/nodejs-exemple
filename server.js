91e42a15a0ada34754322faf7392722592e5565f
Vinicius Eustaquio Cortez <viniciusecortez@outlook.com>
2019-09-12 21:04:22 -0300

    Retirados comentários server js

--- server.js
+++ server.js
@@ -9,9 +9,9 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json({ limit: "5mb" }));
 
-// const rotasDeLivro = require("./routes/livros");
+const rotasDeLivro = require("./routes/livros");
 
-// app.use('/livros', rotasDeLivro);
+app.use('/livros', rotasDeLivro);
 
 app.listen(port, () => {
     console.log(`Servidor está rodando na porta: ${port}`);
